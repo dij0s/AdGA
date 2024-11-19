b97773131a31b29567d45336ca4b8b1b9d806069
Dimitri Imfeld <dimfeld@protonmail.ch>
2024-11-19 09:03:23 +0100

    up

--- mpi_test.py
+++ mpi_test.py
@@ -3,20 +3,30 @@ import time
 import numpy as np
 from optimize_trajectory import GAManager
 from mpi4py import MPI
+import os
+
 
 def main():
-    MPI.Init()
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
+    size = comm.Get_size()
 
     print("Rank: ", rank)
 
-    ranks = []
+    # Prepare the rank to be gathered
+    sendbuf = np.array(rank, dtype='i')
 
-    comm.Gather(rank, ranks, root=0)
+    # Prepare the receive buffer on the root process
+    if rank == 0:
+        recvbuf = np.empty(size, dtype='i')
+    else:
+        recvbuf = None
+
+    # Gather the ranks to the root process
+    comm.Gather(sendbuf, recvbuf, root=0)
 
     if rank == 0:
-        print("Ranks: ", ranks)
+        print("Ranks: ", recvbuf)
 
     MPI.Finalize()
 
--- submit_jobs.sh
+++ submit_jobs.sh
@@ -2,13 +2,15 @@
 #SBATCH --job-name=ga_optimization
 #SBATCH --output=ga_optimization_%A_%a.out
 #SBATCH --error=ga_optimization_%A_%a.err
-#SBATCH --nodelist=calypso[0]             # Use only these 3 nodes
-#SBATCH --nodes=1                             # Use 3 nodes (each node will run some jobs)
-#SBATCH --tasks=10                           # 56 tasks, one per array job (task array size)
+#SBATCH --nodelist=calypso[0,1]             # Use only these 3 nodes
+#SBATCH --nodes=2                             # Use 3 nodes (each node will run some jobs)
+#SBATCH --tasks=10                           
 #SBATCH --time=01:00:00                       # Time limit for each task
 
 
 source /home/dimitri.imfeld/nas_home/AdGA/.venv/bin/activate
 
+whoami
+
 # Run the Python script with the task ID
-mpirun -np 10 /home/dimitri.imfeld/nas_home/AdGA/.venv/bin/python3 optimize_trajectory_slurm.py
+mpirun -np 10 /home/dimitri.imfeld/nas_home/AdGA/.venv/bin/python3 mpi_test.py > out.txt
