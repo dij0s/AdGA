35dc43173c19af0b3f0617c674bbb2e9cef87a0d
dij0s <redrouge03@gmail.com>
2024-11-25 13:44:08 +0100

    WIP..

---
+++ run_simulator.py
@@ -0,0 +1,7 @@
+from simulator import Simulator
+
+import sys
+import numpy as np
+from collections import deque
+
+simulator = Simulator()
---
+++ simulator.py
@@ -0,0 +1,89 @@
+import time
+import threading
+from rallyrobopilot import *
+from collections import deque
+from math import sqrt
+
+class Simulator:
+    """
+    Simulate a series of controls
+    on a car instance through
+    the network
+    """
+    def __init__(self, controls_per_rank, init_positions_per_rank, init_rotations_per_rank, simulation_queue):
+        # TODO: Add init_speed
+        super().__init__()
+
+        self._controls_per_rank = controls_per_rank
+        self._init_positions_per_rank = init_positions_per_rank
+        self._init_rotation_per_rank = init_rotations_per_rank
+        self.simulation_queue = simulation_queue
+        self._recorded_data = []
+
+        # setup backend
+        self._setup_server()
+
+    def start(self):
+        print("[LOG] Waiting for backend setup...")
+
+        # Wait for backend setup with a delay (5 seconds)
+        timer_thread = threading.Timer(5.0, self._start_network_interface)
+        timer_thread.start()
+
+    def _start_network_interface(self):
+        print("[LOG] Starting network interface")
+
+        # Create network interface
+        self.network_interface = NetworkDataCmdInterface(self._collect_data)
+        print("[LOG] Created network interface")
+
+        # Simulate network message sending on a timed interval
+        self.running = True
+        self.network_thread = threading.Thread(target=self._network_loop)
+        self.network_thread.start()
+
+        print("[LOG] Configured network interface")
+
+    def _network_loop(self):
+        while self.running:
+            self.network_interface.send_msg()
+            time.sleep(0.1)  # 100 ms delay
+
+    def _setup_server(self):
+        # Setup track and car
+        track = Track("rallyrobopilot/assets/SimpleTrack/track_metadata.json")
+
+        car = Car(position=self._init_position, speed=self._init_speed, rotation=(0, self._init_rotation, 0))
+        car.sports_car()
+        car.set_track(track)
+
+        RemoteController(car=car, connection_port=7654)
+
+        car.visible = True
+        car.enable()
+
+        track.activate()
+        track.played = True
+
+        print("[LOG] Backend entities are all setup")
+
+    def _collect_data(self, message):
+        self._recorded_data.append(message)
+
+        if self._controls:
+            current_control = self._controls.popleft()
+            for command, start in current_control:
+                self._send_command(command, start)
+        else:
+            print("[LOG] No controls left")
+            self._recorded_data = [sensor.car_position for sensor in self._recorded_data[1:]]
+            print(f"[LOG] Received a total of {len(self._recorded_data)} sensor messages")
+
+            # Stop the network loop and clean up
+            self.running = False
+            self.network_interface.close()
+            self.simulation_queue.put(self._recorded_data)
+
+    def _send_command(self, direction, start):
+        command_types = ["release", "push"]
+        self.network_interface.send_cmd(command_types[start] + " " + direction + ";")
