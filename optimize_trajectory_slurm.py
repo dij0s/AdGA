16000b37ff86fb155a7b564412b3541b5b10892c
Dimitri Imfeld <dimfeld@protonmail.ch>
2024-11-25 15:32:28 +0100

    Add inputs parameters for slurm ga optimizer

--- optimize_trajectory_slurm.py
+++ optimize_trajectory_slurm.py
@@ -6,16 +6,21 @@ from optimize_trajectory import GAManager
 from mpi4py import MPI
 import asyncio
 import aiohttp
+import argparse
 
 _print = print
 def print(*args, **kwargs):
     _print("[%s]" % (datetime.now()),*args, **kwargs)
 
-def main():
-    genetic_algorithm = GAManager(population_size=10, elite_size=2, mutation_rate=0.1)
+def main(trajectories_file, population_size=10, elite_size=2, mutation_rate=0.1, iterations=5, output_file="best_trajectory.npz"):
+    if trajectories_file is None:
+        print("Gimme a trajectory file, bro")
+        return
+
+    genetic_algorithm = GAManager(population_size=population_size, elite_size=elite_size, mutation_rate=mutation_rate)
 
     # Split the recording into trajectories
-    trajectories = genetic_algorithm.split_recording_into_trajectories("records/record_241119111936.npz")
+    trajectories = genetic_algorithm.split_recording_into_trajectories(trajectories_file)
 
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
@@ -36,7 +41,7 @@ def main():
 
     pop = genetic_algorithm.create_population(trajectory[0], trajectory[1])
 
-    evolved_pop, fitnesses = asyncio.run(genetic_algorithm.evolve(pop, trajectory, iterations=5))
+    evolved_pop, fitnesses = asyncio.run(genetic_algorithm.evolve(pop, trajectory, iterations=iterations))
 
     z = zip(evolved_pop, fitnesses)
     best = sorted(z, key=lambda x: x[1], reverse=True)[0]
@@ -62,10 +67,26 @@ def main():
 
     if rank == 0:
         recvbuf = recvbuf.reshape(size, -1)  # Each row corresponds to one process's data
-        np.savez("best_trajectory.npz", recvbuf)
+        np.savez(output_file, recvbuf)
         print(f"Time taken: {time.time() - start_time:.2f} seconds")
 
     MPI.Finalize()
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="AdGA")
+    parser.add_argument("--population_size", "-p", type=int, default=10, help="Population size (number of individuals in each generation)")
+    parser.add_argument("--elite_size", "-e", type=int, default=2, help="Elite size (number of top individuals to keep)")
+    parser.add_argument("--mutation_rate", "-m", type=float, default=0.1, help="Mutation rate (probability of mutation)")
+    parser.add_argument("--iterations", "-i", type=int, default=5, help="Number of iterations to run the genetic algorithm")
+    parser.add_argument("--output-file", "-o", type=str, default="best_trajectory.npz", help="Output file name")
+    parser.add_argument("--trajectories-file", "-t", type=str, help="Input trajectories records file name", required=True)
+    args = parser.parse_args()
+
+    main(
+        trajectories_file=args.trajectories_file,
+        population_size=args.population_size,
+        elite_size=args.elite_size,
+        mutation_rate=args.mutation_rate,
+        iterations=args.iterations,
+        output_file=args.output_file
+    )
