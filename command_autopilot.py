87bbb6269373958e5f99be07e56759c00327f7c8
dij0s <redrouge03@gmail.com>
2024-11-25 11:15:19 +0100

    WIP, zipping ga controls and training data..

--- command_autopilot.py
+++ command_autopilot.py
@@ -1,30 +1,40 @@
 from PyQt6 import QtWidgets
+import pickle, lzma
+
 from data_collector import DataCollectionUI
 
 class OsdNNMsgProcessor:
-    def __init__(self, ga_inputs):
+    def __init__(self, ga_inputs, autopilot_record_file):
         # load commands into queue
         raw_inputs = np.load(ga_inputs)['arr_0'].copy().reshape(-1, 8)
+        with lzma.open(autopilot_record_file, "rb") as file:
+            _, ap_positions, ap_speeds, ap_angles = pickle.load(file)
 
         per_rank_commands = {}
 
         for raw_input in raw_inputs:
             rank = int(raw_input[0])
-
             if rank not in per_rank_commands:
-                per_rank_commands[rank] = []
+                per_rank_commands[rank] = deque([])
 
-            per_rank_commands[rank].append(map(lambda x: int(x), raw_input[1:5]))
-        # only take n-overlap first
-        # commands to not handle overlap
-        # self._commands = deque([map(lambda x: int(x), raw_input[1:5]) for raw_input in raw_inputs])
-        # print(deque([raw_input[5:] for raw_input in raw_inputs]))
+            per_rank_commands[rank].append((map(lambda x: int(x), raw_input[1:5])))
 
-        print([len(per_rank_commands[rank]) for rank in per_rank_commands.keys()])
+        # add init_speed and init_angle
+        # to the per rank commands
+        for rank in per_rank_commands.keys():
+            init_index = rank * 10
+            per_rank_commands[rank] = (
+                per_rank_commands[rank],
+                ap_positions[init_index],
+                ap_angles[init_index]
+            )
+
+        self._per_rank_commands = per_rank_commands
+
         self._directions = ["forward", "back", "left", "right"]
 
     def process_message(self, _, data_collector):
-        current_command = self._commands.popleft()
+        # current_command = self._commands.popleft()
         for command, start in (
             map(
                 lambda ic: (self._directions[ic[0]], ic[1]), # map command to remote instruction
@@ -44,7 +54,7 @@ if  __name__ == "__main__":
 
     app = QtWidgets.QApplication(sys.argv)
 
-    nn_brain = OsdNNMsgProcessor(sys.argv[1])
+    nn_brain = OsdNNMsgProcessor(sys.argv[1], sys.argv[2])
     data_window = DataCollectionUI(nn_brain.process_message)
     data_window.show()
 
