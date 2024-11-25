8abea9f727958c87965a3bf19eb01d45f4dba346
Dimitri Imfeld <dimfeld@protonmail.ch>
2024-11-25 11:23:50 +0100

    Update submit_jobs.sh

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -2,12 +2,11 @@
 #SBATCH --job-name=ga_optimization
 #SBATCH --output=ga_optimization_%A_%a.out
 #SBATCH --error=ga_optimization_%A_%a.err
-#SBATCH --nodelist=calypso[0,2]             # Use only these 3 nodes
-#SBATCH --nodes=2                             # Use 3 nodes (each node will run some jobs)
+#SBATCH --nodelist=calypso[3]             
+#SBATCH --nodes=1                             
 #SBATCH --tasks=36                          
-
 
 source ~/.venv/bin/activate
 
 # Run the Python script with the task ID
-mpirun -np 60  ~/.venv/bin/python3 optimize_trajectory_slurm.py
+mpirun -np 36  python optimize_trajectory_slurm.py
