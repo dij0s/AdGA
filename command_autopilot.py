820c15913d37b5f8edd7b6ac0122097b22f93d8e
dij0s <redrouge03@gmail.com>
2024-11-25 10:32:36 +0100

    WIP, implementing per trajectory simulation..

--- command_autopilot.py
+++ command_autopilot.py
@@ -1,32 +1,35 @@
-
-
 from PyQt6 import QtWidgets
-
 from data_collector import DataCollectionUI
-"""
-This file is provided as an example of what a simplistic controller could be done.
-It simply uses the DataCollectionUI interface zo receive sensing_messages and send controls.
 
-/!\ Be warned that if the processing time of NNMsgProcessor.process_message is superior to the message reception period, a lag between the images processed and commands sent.
-One might want to only process the last sensing_message received, etc. 
-Be warned that this could also cause crash on the client side if socket sending buffer overflows
-
-/!\ Do not work directly in this file (make a copy and rename it) to prevent future pull from erasing what you write here.
-"""
 class OsdNNMsgProcessor:
     def __init__(self, ga_inputs):
         # load commands into queue
         raw_inputs = np.load(ga_inputs)['arr_0'].copy().reshape(-1, 8)
-        
+
+        per_rank_commands = {}
+
+        for raw_input in raw_inputs:
+            rank = int(raw_input[0])
+
+            if rank not in per_rank_commands:
+                per_rank_commands[rank] = []
+
+            per_rank_commands[rank].append(map(lambda x: int(x), raw_input[1:5]))
         # only take n-overlap first
         # commands to not handle overlap
-        self._commands = deque([map(lambda x: int(x), raw_input[1:5]) for raw_input in raw_inputs])
-        
+        # self._commands = deque([map(lambda x: int(x), raw_input[1:5]) for raw_input in raw_inputs])
+        # print(deque([raw_input[5:] for raw_input in raw_inputs]))
+
+        print([len(per_rank_commands[rank]) for rank in per_rank_commands.keys()])
         self._directions = ["forward", "back", "left", "right"]
 
     def process_message(self, _, data_collector):
         current_command = self._commands.popleft()
-        for command, start in map(lambda ic: (self._directions[ic[0]], ic[1]), enumerate(current_command)):
+        for command, start in (
+            map(
+                lambda ic: (self._directions[ic[0]], ic[1]), # map command to remote instruction
+                enumerate(current_command)
+            )):
             data_collector.onCarControlled(command, start)
 
 if  __name__ == "__main__":
