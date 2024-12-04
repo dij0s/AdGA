6d47a08d458d135ebade1976be4fbd3ff3ccfc49
Dimitri Imfeld <dimfeld@protonmail.ch>
2024-12-04 13:00:10 +0100

    Use session pooling in api orchestrator

--- api_orchestrator.py
+++ api_orchestrator.py
@@ -3,8 +3,24 @@ from flask import Flask, jsonify, request
 import requests
 import subprocess
 import json
+from requests.adapters import HTTPAdapter
+
+from urllib3 import Retry
 
 app = Flask(__name__)
+
+def create_requests_session(retries=3, backoff_factor=0.3):
+    session = requests.Session()
+    retry_strategy = Retry(
+        total=retries,
+        status_forcelist=[429, 500, 502, 503, 504],
+        method_whitelist=["POST"],
+        backoff_factor=backoff_factor
+    )
+    adapter = HTTPAdapter(max_retries=retry_strategy)
+    session.mount("http://", adapter)
+    session.mount("https://", adapter)
+    return session
 
 class ApiOrchestratorSingleton:
     instance = None
@@ -17,6 +33,8 @@ class ApiOrchestratorSingleton:
         self.max_simultaneous_requests = max_simultaneous_requests
         self.pods_usage = {}
         self.pods = []
+
+        self.session = create_requests_session()
 
         with open('sim-pods.json', 'r') as f:
             pods_info = json.loads(f.read())
@@ -48,7 +66,7 @@ class ApiOrchestratorApi:
 
         try:
             print(f"Sending request to {min_pod}")
-            response = requests.post(f"http://{min_pod}:5000/api/simulate", json=data)
+            response = orchestrator.session.post(f"http://{min_pod}:5000/api/simulate", json=data)
         except Exception as e:
             print(f"Failed to communicate with the simulator")
             print(e)
